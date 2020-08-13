b095cf1455f2fa456e8d8d6167d01c7995ea65f9
nikikalwar <nikikumar27@gmail.com>
2020-08-13 18:14:07 +0530

    calculator

---
+++ cal.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,sum=0;
+    cout<<"enter x \n"
+    cin>>x;
+    cout<<"Enter y\n"
+    cin>>y;
+    sum=x+y;
+    cout<<"The sum is "<<sum;
+    return 0;
+}
